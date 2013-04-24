9c858ad5ad801a1a0534c297e81d55e1d0686193
msg-ios <msg.maruko@gmail.com>
2013-04-24 18:49:14 -0300

    Initial Commit

---
+++ FoursquareExample/FoursquareAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  FoursquareAppDelegate.h
+//  FoursquareExample
+//
+//  Created by Marco S. Graciano on 4/24/13.
+//  Copyright (c) 2013 Marco Graciano. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FoursquareAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
